1fd17017de6a38a55fb2016a186a6ca0f28fc66d
mediDEU <mehdivasef@gmail.com>
2025-06-07 18:08:49 +0200

    Update and rename providers.tf to backend.tf

---
+++ backend.tf
@@ -0,0 +1,21 @@
+terraform {
+ required_providers {
+   aws = {
+     source = "hashicorp/aws"
+   }
+ }
+#############################
+
+provider "aws" {
+
+ region = "eu-central-1"
+
+}
+#############################################################################
+terraform {
+  backend "s3" {
+    bucket = "github-oidc-terraform-tfstate-medi"
+    key    = "path/to/my/key"
+    region = "eu-central-1"
+  }
+}
--- providers.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "github-oidc-terraform-tfstate-medi"
-    key    = "path/to/my/key"
-    region = "eu-central-1"
-  }
-}
